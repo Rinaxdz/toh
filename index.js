1de6fb5c970c83ea3e6c384b4472484eb8ddd967
Rinaxdz <91366054+Rinaxdz@users.noreply.github.com>
2023-10-03 15:03:54 +0800

    Add files via upload
    2

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let moves = [];
+let moveCount = 0;
 
 function solve() {
     moves = [];
+    moveCount = 0;
     const numDisks = parseInt(document.getElementById('numDisks').value);
     towerOfHanoi(numDisks, 'A', 'C', 'B');
     animateMoves();
@@ -20,17 +22,31 @@ function towerOfHanoi(n, source, target, auxiliary) {
 
 function animateMoves() {
     let delay = 0;
+    const moveList = document.getElementById('moveList');
+    
     moves.forEach((move, index) => {
         const disk = document.querySelector(`#${move.source} .disk`);
         const targetTower = document.getElementById(move.target);
 
         setTimeout(() => {
             targetTower.appendChild(disk);
-        }, index * 1000); 
+            moveCount++;
+            updateMoveList(moveCount, move.source, move.target);
+        }, index * 1000); // Adjust this value for the speed of animation
     });
 }
 
-document.getElementById('solve').addEventListener('click', solve);
+function updateMoveList(moveNumber, source, target) {
+    const moveList = document.getElementById('moveList');
+    const listItem = document.createElement('li');
+    listItem.textContent = `Move ${moveNumber}: Move a disk from ${source} to ${target}`;
+    moveList.appendChild(listItem);
+}
 
+document.getElementById('towerForm').addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevent the form from actually submitting
+    solve();
+});
 
+// Initial setup with 3 disks
 solve();
